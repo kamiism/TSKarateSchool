08f2379c437858d589e232f654501f5d982054e5
anirbandotdev <anirbaniitian@gmail.com>
2026-06-12 20:28:53 +0530

    Configure rate limiter

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,16 @@
-import express, { type Request, type Response } from "express";
+import express, {
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
 import cors from "cors";
-import { CORS_ORIGIN } from "./config/env.js";
+import {
+  CORS_ORIGIN,
+  RATE_LIMIT_MAX_REQUESTS,
+  RATE_LIMIT_WINDOW_MS,
+} from "./config/env.js";
+import router from "./routes/router.ts";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
@@ -13,6 +23,17 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+
+const limiter = rateLimit({
+  windowMs: RATE_LIMIT_WINDOW_MS,
+  limit: RATE_LIMIT_MAX_REQUESTS,
+  handler: (req: Request, res: Response, next: NextFunction) => {}, // TODO: I will update this whenever i will setup response handler
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use("/api");
+app.use("/api/v1", router);
 
 app.get("/health", (req: Request, res: Response) => {
   res.status(200).json({
--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -4,5 +4,9 @@ config({
   path: "./.env",
 });
 
-export const PORT = process.env.PORT;
-export const CORS_ORIGIN = process.env.CORS_ORIGIN;
+export const PORT = Number(process.env.PORT);
+export const CORS_ORIGIN = process.env.CORS_ORIGIN!;
+export const RATE_LIMIT_WINDOW_MS = Number(process.env.RATE_LIMIT_WINDOW_MS);
+export const RATE_LIMIT_MAX_REQUESTS = Number(
+  process.env.RATE_LIMIT_MAX_REQUESTS,
+);
