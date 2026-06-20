ffa40b5749a14f00f33ee6a6b764376d9272b0e8
anirbandotdev <anirbaniitian@gmail.com>
2026-06-20 14:19:17 +0530

    add quiz model

---
+++ backend/src/models/Quiz.ts
@@ -0,0 +1,60 @@
+import mongoose, { Document } from "mongoose";
+
+type QuizType = {
+  question: string;
+  options: string[];
+  answer_idx: number;
+};
+
+interface IQuiz extends Document {
+  name: string;
+  quizzes: QuizType[];
+  start_hour: number;
+  end_hour: number;
+}
+
+const quizSchema = new mongoose.Schema<IQuiz>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    quizzes: [
+      {
+        type: new mongoose.Schema<QuizType>(
+          {
+            question: {
+              type: String,
+              required: true,
+              trim: true,
+            },
+            options: [
+              {
+                type: String,
+                trim: true,
+              },
+            ],
+            answer_idx: {
+              type: Number,
+              required: true,
+            },
+          },
+          { _id: false },
+        ),
+        required: true,
+      },
+    ],
+    start_hour: {
+      type: Number,
+      required: true,
+    },
+    end_hour: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Quiz = mongoose.model<IQuiz>("Quiz", quizSchema);
