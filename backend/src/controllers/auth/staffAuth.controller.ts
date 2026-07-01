071c835fde9b098625074e661faca30b7c282edb
anirbandotdev <anirbaniitian@gmail.com>
2026-07-01 12:05:24 +0530

    add login logic

--- backend/src/controllers/auth/staffAuth.controller.ts
+++ backend/src/controllers/auth/staffAuth.controller.ts
@@ -7,6 +7,7 @@ import {
   STATUS_CODES,
 } from "../../config/constants.ts";
 import { Staff } from "../../models/Staff.ts";
+import bcrypt from "bcrypt";
 
 export class StaffAuthController {
   async create(req: Request, res: Response, next: NextFunction) {
@@ -42,7 +43,50 @@ export class StaffAuthController {
     sendSuccess(res, STATUS_CODES.CREATED, API_RESPONSE_MESSAGES.CREATED);
   }
 
-  async login(req: Request, res: Response, next: NextFunction) {}
+  async login(req: Request, res: Response, next: NextFunction) {
+    const { identifier, password } = req.body;
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/;
+    let user = null;
+    if (regex.test(identifier)) {
+      user = await Staff.findOne({ email: identifier });
+    } else {
+      user = await Staff.findOne({ username: identifier });
+    }
+
+    if (!user) {
+      return sendError(
+        res,
+        STATUS_CODES.NOT_FOUND,
+        API_RESPONSE_MESSAGES.NOT_FOUND,
+      );
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return sendError(res, STATUS_CODES.UNAUTHORISED, "Invalid password");
+    }
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save();
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    sendSuccess(res, STATUS_CODES.OK, API_RESPONSE_MESSAGES.SUCCESS, {
+      accessToken,
+    });
+  }
 }
 
 export const staffAuthController = new StaffAuthController();
