8b831a813f0d4e5d3d6a33a44020918682d3ba62
anirbandotdev <anirbaniitian@gmail.com>
2026-07-07 22:13:48 +0530

    minor fix

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {};
+    fetchData();
+  }, []);
+
+  return <AuthContext.Provider value={user , loading}>{children}</AuthContext.Provider>;
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+}
