118ea78752ceb161a9b1167a7ea8e0bb7dbec4de
Kamii <kumarroynilanjan@gmail.com>
2026-07-10 23:24:44 +0530

    fix #3

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,11 +1,25 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import "./ProtectedRoute.css";
 
 export function ProtectedRoute({ role = "user", children }) {
   const { user, loading, staff } = useAuth();
 
   if (loading) {
-    return <div>Loading...</div>; // Frontend guy please change it
+    return (
+      <div className="protected-route-loader">
+        <div className="spinner">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+    );
   }
   if ((role == "user" && user) || (role == "staff" && staff)) {
     return children;
--- frontend/src/components/Redirect.jsx
+++ frontend/src/components/Redirect.jsx
@@ -1,10 +1,24 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import "./ProtectedRoute.css";
 
 export function Redirect({ to, children }) {
   const { user, loading, staff } = useAuth();
   if (loading) {
-    return <div>Loading....</div>; // Frontend guy please change this
+    return (
+      <div className="protected-route-loader">
+        <div className="spinner">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+    );
   }
   
   if (user || staff) {
